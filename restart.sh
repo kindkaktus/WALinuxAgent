06d3e041771c7932c981c382267bccb31dfe3e9c
Andrei Korostelev <andrei@korostelev.net>
2014-03-10 08:22:59 -0700

    Pretty printing

--- restart.sh
+++ restart.sh
@@ -3,9 +3,12 @@
 set -e;
 
 if pkill -0 waagent ; then
+    echo "Stopping waagent..."
     pkill -9 waagent
 fi
 
 cp waagent /usr/sbin/
+echo "Installing waagent..."
 /usr/sbin/waagent -install
+echo "Starting waagent..."
 /usr/sbin/waagent -daemon
